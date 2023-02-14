2fa1434004c7da300e1a4be173221d8cf8929e28
Don Livingston <donlivingston@gmail.com>
2023-02-14 14:40:53 -0500

    plus-minus challenge

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello");
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+	inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+	inputString = inputString.split('\n');
+	
+	main();
+});
+
+function readLine() {
+	return inputString[currentLine++];
+}
+
+/*
+* Complete the 'plusMinus' function below.
+*
+* The function accepts INTEGER_ARRAY arr as parameter.
+*/
+
+function plusMinus(arr) {
+	// Write your code here
+	let posNum = 0
+	let negNum = 0
+	let zeroNum = 0
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] > 0) { ++posNum }
+		else if(arr[i] < 0) { ++negNum }
+		else if(arr[i] === 0) { ++zeroNum }
+	}
+	process.stdout.write((posNum/arr.length).toFixed(6) + '\n')
+	process.stdout.write((negNum/arr.length).toFixed(6) + '\n')
+	process.stdout.write((zeroNum/arr.length).toFixed(6) + '\n')
+}
+
+function main() {
+	const n = parseInt(readLine().trim(), 10);
+	
+	const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+	
+	plusMinus(arr);
+}
